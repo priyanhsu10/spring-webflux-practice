e887909ca2c669c3da5f6c88e3912db17ffff17e
priyanshu10 <priyanshuparate@gmail.com>
2025-03-14 14:15:13 +0530

    first commit

---
+++ src/main/java/com/pro/webflux_playgroud/WebfluxPlaygroudApplication.java
@@ -0,0 +1,13 @@
+package com.pro.webflux_playgroud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxPlaygroudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxPlaygroudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pro/webflux_playgroud/WebfluxPlaygroudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.webflux_playgroud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPlaygroudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
