9b91ff872b35b02eec475e57587849c49d45117e
priyanshu10 <priyanshuparate@gmail.com>
2025-03-15 18:31:40 +0530

    pageable implemetation

--- src/main/java/com/pro/playground/sec02/repository/ProductRepository.java
+++ src/main/java/com/pro/playground/sec02/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package com.pro.playground.sec02.repository;
 
 import com.pro.playground.sec02.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
+
 public interface ProductRepository extends ReactiveCrudRepository<Product ,Integer> {
     Flux<Product>  findByPriceBetween(int from, int to);
+    Flux<Product> findBy(Pageable pageable);
 }
--- src/test/java/com/pro/playground/test/sec02/Lec02ProductRepositoryTest.java
+++ src/test/java/com/pro/playground/test/sec02/Lec02ProductRepositoryTest.java
@@ -1,12 +1,17 @@
 package com.pro.playground.test.sec02;
 
 import com.pro.playground.sec02.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import reactor.test.StepVerifier;
+
 
 
 public class Lec02ProductRepositoryTest extends  AbstractTest{
@@ -20,6 +25,19 @@ public class Lec02ProductRepositoryTest extends  AbstractTest{
                 .doOnNext(x->log.info("{}",x))
                 .as(StepVerifier::create)
                 .expectNextCount(6)
+                .expectComplete()
+                .verify();
+    }
+    @Test
+    public  void pagable(){
+        Pageable p= PageRequest.of(0,3)
+                .withSort(Sort.by("price").ascending());
+        this.productRepository.findBy(p)
+                .doOnNext(x->log.info("{}",x))
+                .as(StepVerifier::create)
+                .assertNext(x-> Assertions.assertEquals(200,x.getPrice()))
+                .assertNext(x-> Assertions.assertEquals(250,x.getPrice()))
+                .assertNext(x-> Assertions.assertEquals(300,x.getPrice()))
                 .expectComplete()
                 .verify();
     }
